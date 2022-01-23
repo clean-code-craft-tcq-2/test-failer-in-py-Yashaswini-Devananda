2fde8a82aecf0c4ac82ee72356a6be77d26e78ac
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-01-23 19:11:35 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -4,8 +4,8 @@ def print_color_map():
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            color_code_pairs.(f'{i * 5 + j} | {major} | {minor}')
-            print(f'{i * 5 + j} | {major} | {minor}')
+            color_code_pairs.append((f'{i * 5 + j} | {major} | {minor}'))
+            print(color_code_pairs[i * 5 + j])
     return len(major_colors) * len(minor_colors)
 
 
