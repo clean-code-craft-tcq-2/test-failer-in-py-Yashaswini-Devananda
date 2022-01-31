8b7948e7a273aea8198d3e9725cf700df6bdf5f8
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-01-31 22:52:06 +0530

    Update test_alerter.py

--- test_alerter.py
+++ test_alerter.py
@@ -1,11 +1,22 @@
 import alerter
+stub_control_ok = True
 
 def network_alert_stub(celcius):
     print(f'Stub ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
-    return 500 
-  
+    if stub_control_ok == True:
+        return 200
+    else:
+        return 500 
+     
+alerter.alert_in_celcius(400.5)
+alerter.alert_in_celcius(303.6)
+print(f'{alerter.alert_failure_count} alerts failed.')
+print('All is well (maybe!)')
+assert(alerter.alert_failure_count == 2) 
+
+stub_control_ok = False
 alerter.alert_in_celcius(400.5)
 alerter.alert_in_celcius(303.6)
 print(f'{alerter.alert_failure_count} alerts failed.')
